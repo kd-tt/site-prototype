d36df4c0a0e1f35f73d8b672cc63fcecabe65dfa
kd-tt <koutt9228@outlook.jp>
2020-03-06 22:11:03 +0900

    '2回目：ハンバーガーメニュー'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,6 @@
-$(function(){
-    $(".btn-gnavi").on("click", function(){
-        // ハンバーガーメニューの位置を設定
-        var rightVal = 0;
-        if($(this).hasClass("open")) {
-            // 位置を移動させメニューを開いた状態にする
-            rightVal = -300;
-            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-            $(this).removeClass("open");
-        } else {
-            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-            $(this).addClass("open");
-        }
- 
-        $("#global-navi").stop().animate({
-            right: rightVal
-        }, 200);
-    });
+const ham = document.getElementById('ham');
+const menu_wrapper = document.getElementById('menu_wrapper');
+ham.addEventListener('click', function() {
+	ham.classList.toggle('clicked');
+	menu_wrapper.classList.toggle('clicked');
 });
